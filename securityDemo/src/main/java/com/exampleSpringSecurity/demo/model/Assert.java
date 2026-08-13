3eb96070c9c56fadffa8cc2322adae2be9d64572
Kumar <krsaket.dev@gmail.com>
2026-08-13 19:15:24 +0530

    test

---
+++ securityDemo/src/main/java/com/exampleSpringSecurity/demo/model/Assert.java
@@ -0,0 +1,11 @@
+package com.exampleSpringSecurity.demo.model;
+
+public class Assert
+{
+    private  Long id;
+    private  String assetName;
+    private  String assetType;
+    private  String status;
+    private  String location;
+
+}
